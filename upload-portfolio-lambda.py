33ef1e66de274a098d14f66e820374b646fc048d
elwaz <warwick@automatic.systems>
2017-11-05 19:55:41 +1000

    update port deployment

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,17 +5,20 @@ import boto3
 from botocore.client import Config
 
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-port_bucket = s3.Bucket('elwaz.net')
-build_bucket = s3.Bucket('port.elwaz')
+    port_bucket = s3.Bucket('elwaz.net')
+    build_bucket = s3.Bucket('port.elwaz')
 
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfolio.zip', portfolio_zip)
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfolio.zip', portfolio_zip)
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        port_bucket.upload_fileobj(obj, nm,
-            ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        port_bucket.Object(nm).Acl().put(ACL='public-read')
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            port_bucket.upload_fileobj(obj, nm,
+                ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            port_bucket.Object(nm).Acl().put(ACL='public-read')
+
+    return 'Job done!'
